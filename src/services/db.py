cdb82ce6530ff077094068f3c412580d0bd3acfc
danilojezernik <dani.jezernik@gmail.com>
2023-12-04 09:06:28 +0100

    First commit

--- src/services/db.py
+++ src/services/db.py
@@ -9,16 +9,17 @@ from src.database.hsalen.public import logging_public
 client = MongoClient(env.DB_CONNECTION_LOGGING)
 
 # HYPNOSIS STUDIO ALEN COLLECTION
-proces = client[env.DB_PROCES_LOGGING]
+private_hsa = client[env.DB_PROCES_PRIVATE_HSA_LOGGING]
+public_hsa = client[env.DB_PROCES_PUBLIC_HSA_LOGGING]
 
 
 def drop_log():
-    proces.logging_private.drop()
-    proces.logging_public.drop()
+    private_hsa.logging_private.drop()
+    public_hsa.logging_public.drop()
     pass
 
 
 def seed_log():
-    proces.logging_private.insert_many(logging_private)
-    proces.logging_public.insert_many(logging_public)
+    private_hsa.logging_private.insert_many(logging_private)
+    public_hsa.logging_public.insert_many(logging_public)
     pass
