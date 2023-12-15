8656561a25c0984d41b574765197d8aa87904a27
danilojezernik <dani.jezernik@gmail.com>
2023-12-15 05:50:15 +0100

    Login added

--- src/database/hsalen/backend.py
+++ src/database/hsalen/backend.py
@@ -8,7 +8,6 @@ backend_logs = [
         domain='BACKEND',
         content='content',
         client_host='IPADDRESS',
-        city='Mozirje',
         datum_vnosa=datetime.datetime.now()
     ).dict(by_alias=True)
 ]
--- src/domain/hsalen/backend.py
+++ src/domain/hsalen/backend.py
@@ -10,6 +10,5 @@ class BackendLogs(BaseModel):
     route_action: str
     domain: str
     client_host: str
-    city: str
     content: str
     datum_vnosa: datetime.datetime = Field(default_factory=datetime.datetime.now)
