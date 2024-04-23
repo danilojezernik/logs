3e07b6c2439600be08f67db1bdc32b868145d7ac
Danilo Jezernik <96477921+danilojezernik@users.noreply.github.com>
2024-04-23 12:40:12 +0200

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     # Drop the database and seed it
     # db.drop_log()
     # db.seed_log()
+    
 
     # Run the FastAPI application using Uvicorn server
     uvicorn.run(app, host="0.0.0.0", port=env.PORT)
